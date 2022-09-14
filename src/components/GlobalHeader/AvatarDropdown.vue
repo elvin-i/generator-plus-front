c6c8a9c121a7964c4acb66bf1229b55175afd8ca
zhanglei001 <zhanglei001@buukle.top>
2022-09-14 21:13:27 +0800

    fix

--- src/components/GlobalHeader/AvatarDropdown.vue
+++ src/components/GlobalHeader/AvatarDropdown.vue
@@ -56,7 +56,7 @@ export default {
         content: this.$t('layouts.usermenu.dialog.content'),
         onOk: () => {
           request({
-            url: '/auth/logout',
+            url: process.env.VUE_APP_LOGIN_CUBE_SERVER_URL,
             method: 'get',
             dataType: 'json'
           }).then(res => {
